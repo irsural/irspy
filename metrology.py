918a3b4f0d2c2575208e57514872e78c3a9fe483
feelinflight <kolpacc123@mail.ru>
2020-07-14 17:37:52 +0500

    Добавил модуль built_in_extensions. Доработал класс MovingAverage

--- metrology.py
+++ metrology.py
@@ -6,22 +6,27 @@ class StabilityControl:
 
 
 class MovingAverage:
-    def __init__(self, a_window_size: int):
-        assert a_window_size != 0, "Размер окна не может быть равен нулю"
-
+    def __init__(self, a_window_size: int = 0):
         self.__window_size = a_window_size
-        self.__values = deque(maxlen=a_window_size)
         self.__sum = 0
+
+        if self.__window_size:
+            self.__values = deque(maxlen=a_window_size)
+        else:
+            self.__values = deque()
 
     def reset(self, a_window_size=None):
         if a_window_size:
             self.__window_size = a_window_size
-
-        self.__values = deque(maxlen=self.__window_size)
         self.__sum = 0
 
+        if self.__window_size:
+            self.__values = deque(maxlen=a_window_size)
+        else:
+            self.__values = deque()
+
     def add(self, a_value: float):
-        if len(self.__values) == self.__window_size:
+        if self.__window_size > 0 and len(self.__values) == self.__window_size:
             popped_value = self.__values[0]
             self.__sum -= popped_value
         self.__sum += a_value
