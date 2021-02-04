7f7dc10c4553ea4c5e65123a3ae0fc10cbd331d7
feelinglight <kolpacc123@mail.ru>
2021-02-04 15:21:47 +0500

    Добавил функцию для создания ts-файлов перевода

--- qt/ui_to_py.py
+++ qt/ui_to_py.py
@@ -35,18 +35,50 @@ def __convert_gui(convert_cmd: str, ext_in: str, ext_out: str, path_in=".", path
     except FileNotFoundError:
         hashes = {}
 
-    for r, d, f in os.walk(path_in):
-        for file in f:
-            if ext_in in file:
-                ui_filename = "{0}/{1}".format(path_in, file)
-                ui_current_hash = __get_file_hash(ui_filename)
-                ui_prev_hash = "" if ui_filename not in hashes.keys() else hashes[ui_filename]
+    for file in os.listdir(path_in):
+        if ext_in in file:
+            ui_filename = "{0}/{1}".format(path_in, file)
+            ui_current_hash = __get_file_hash(ui_filename)
+            ui_prev_hash = "" if ui_filename not in hashes.keys() else hashes[ui_filename]
 
-                py_filename = "{0}/{1}".format(path_out, file.replace(ext_in, ext_out))
+            py_filename = "{0}/{1}".format(path_out, file.replace(ext_in, ext_out))
 
-                if ui_prev_hash != ui_current_hash or not os.path.isfile(py_filename):
-                    hashes[ui_filename] = ui_current_hash
-                    os.system(convert_cmd.format(_in=ui_filename, _out=py_filename))
+            if ui_prev_hash != ui_current_hash or not os.path.isfile(py_filename):
+                hashes[ui_filename] = ui_current_hash
+                os.system(convert_cmd.format(_in=ui_filename, _out=py_filename))
+
+    with open(hashes_path, 'w') as hashes_file:
+        json.dump(hashes, hashes_file)
+
+
+def create_translate(a_py_files_folder: str, a_ts_files_folder):
+    """
+    Создает ts-файлы с помощью pylupdate5 из py-файлов qt-форм. Полученные файлы можно открыть в QtLinguist, чтобы
+    сгенерировать qm-файл для QTranslator
+    :param a_py_files_folder: Каталог с py-файлами
+    :param a_ts_files_folder: Каталог, в который будут помещены ts-файлы
+    """
+    hashes_path = "{0}/{1}".format(a_py_files_folder, HASHES_FILE)
+    try:
+        with open(hashes_path, 'r') as hashes_file:
+            hashes = json.load(hashes_file)
+    except FileNotFoundError:
+        hashes = {}
+
+    if not os.path.isdir(a_ts_files_folder):
+        os.makedirs(a_ts_files_folder)
+
+    for file in os.listdir(a_py_files_folder):
+        if ".py" in file:
+            py_filename = "{0}/{1}".format(a_py_files_folder, file)
+            py_current_hash = __get_file_hash(py_filename)
+            py_prev_hash = "" if py_filename not in hashes.keys() else hashes[py_filename]
+
+            ts_filename = "{0}/{1}".format(a_ts_files_folder, file.replace(".py", ".ts"))
+
+            if py_prev_hash != py_current_hash or not os.path.isfile(ts_filename):
+                hashes[py_filename] = py_current_hash
+                os.system("pylupdate5 {} -ts {}".format(py_filename, ts_filename))
 
     with open(hashes_path, 'w') as hashes_file:
         json.dump(hashes, hashes_file)
@@ -60,3 +92,4 @@ def __get_file_hash(a_filename):
 if __name__ == "__main__":
     convert_ui("../../ui", "../../ui/py")
     convert_resources("../../resources", "../../")
+    # create_translate("../../upms_1v_pc/ui/py", "../../upms_1v_pc/ui/translates")
