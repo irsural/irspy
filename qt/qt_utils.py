4c4e1f977c2c3ed729a8e4d8476e6e3b93424162
qucals <totalboy@outlook.com>
2022-02-28 18:49:28 +0500

    Правки под астру
    Signed-off-by: qucals <totalboy@outlook.com>

--- qt/qt_utils.py
+++ qt/qt_utils.py
@@ -1,4 +1,3 @@
-from math import isclose
 import logging
 
 from PyQt5 import QtCore, QtWidgets, QtGui
@@ -9,16 +8,6 @@ import irspy.constants as constants
 QSTYLE_COLOR_WHITE = "background-color: rgb(255, 255, 255);"
 QSTYLE_COLOR_YELLOW = "background-color: rgb(250, 250, 170);"
 QSTYLE_COLOR_RED = "background-color: rgb(245, 206, 203);"
-
-
-def update_edit_color(a_actual_value, a_current_value, a_edit: QtWidgets.QLineEdit):
-    try:
-        if isclose(a_actual_value, float(a_current_value), rel_tol=constants.FLOAT_EPSILON):
-            a_edit.setStyleSheet(QSTYLE_COLOR_WHITE)
-        else:
-            a_edit.setStyleSheet(QSTYLE_COLOR_YELLOW)
-    except ValueError:
-        a_edit.setStyleSheet(QSTYLE_COLOR_RED)
 
 
 def get_wheel_steps(event: QtGui.QWheelEvent):
