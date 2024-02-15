a1ba8a86de65e59cea3cf06a7079dddb5e19bda8
PryamininTV <ghzvbyby@gmail.com>
2024-02-15 09:09:42 +0500

    Добавлена возможность рекурсивного обхода каталогов при создании словарей

--- irspy/qt/ui_to_py.py
+++ irspy/qt/ui_to_py.py
@@ -39,11 +39,14 @@ def convert_resources(path_in: str = ".", path_out: str = "."):
         __qrc_to_py([str(qrc_filename)], str(py_filename))
 
 
-def __old_files_with_extension(path_in, ext_in: str, path_out, ext_out: str) -> Tuple[Path, Path]:
+def __old_files_with_extension(path_in, ext_in: str, path_out, ext_out: str, recursively: bool = False) -> Tuple[Path, Path]:
     path_in = Path(path_in)
     path_out = Path(path_out)
-
-    for file in os.listdir(str(path_in)):
+    if recursively:
+        files = (os.path.join(root, f) for root, dirs, files in os.walk(str(path_in)) for f in files)
+    else:
+        files = os.listdir(str(path_in))
+    for file in files:
         if file.endswith(ext_in):
             file = Path(file)
             ui_filename = path_in/file
@@ -67,7 +70,7 @@ def __qrc_to_py(qrc_filename, py_filename):
     return library.output(py_filename)
 
 
-def create_translate(a_py_files_folder: str, a_ts_file_path: str):
+def create_translate(a_py_files_folder: str, a_ts_file_path: str, recursively: bool = False):
     """
     Создает ts-файлы с помощью pylupdate5 из py-файлов qt-форм. Полученные файлы можно открыть в QtLinguist, чтобы
     сгенерировать qm-файл для QTranslator
@@ -76,7 +79,7 @@ def create_translate(a_py_files_folder: str, a_ts_file_path: str):
     """
 
     py_files_list = []
-    for py_filename, ts_filename in __old_files_with_extension(a_py_files_folder, ".py", a_ts_file_path, ".ts"):
+    for py_filename, ts_filename in __old_files_with_extension(a_py_files_folder, ".py", a_ts_file_path, ".ts", recursively):
         py_files_list.append(str(py_filename))
         os.system("pylupdate5 {} -ts {}".format(" ".join(py_files_list), a_ts_file_path))
 
