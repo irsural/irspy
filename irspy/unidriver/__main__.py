7e918a43325a57109e2b5e81b1a6565260892753
PryamininTV <ghzvbyby@gmail.com>
2024-02-21 11:33:10 +0500

    Доработан пример

--- irspy/unidriver/__main__.py
+++ irspy/unidriver/__main__.py
@@ -1,4 +1,4 @@
-from irspy.unidriver.netvar import NetVarFabric, NetVar, NetVarCTypes, NetVarIndex
+from irspy.unidriver.netvar import NetVarFabric, NetVar, NetVarCTypes, NetVarIndex, NetVarRepo
 from irspy.unidriver.unidriver import UnidriverDLLWrapper, UnidriverDeviceBuilder, UnidriverScheme, \
     UnidriverIO, UnidriverDeviceFabric, ParamTypes
 
@@ -10,8 +10,8 @@ def builder_main() -> None:
     scheme = UnidriverScheme(unidriver_dll)
     param = scheme.param(6)
     print(param, scheme.string(param.name), type(param.default))
-    builder_handle = dev_builder.make_builder(0, 0)
-    dev_builder.set_param(builder_handle, 0, ParamTypes.STRING, '192.168.0.86')
+    builder_handle = dev_builder.make_builder(group_scheme_index=0, builder_scheme_index=0)
+    dev_builder.set_param(builder_handle, 0, ParamTypes.STRING, '192.168.0.81')
     dev_builder.set_param(builder_handle, 1, ParamTypes.STRING, '5006')
     dev_builder.set_param(builder_handle, 7, ParamTypes.INT32, 32)
     device_handle = dev_builder.apply(builder_handle)
@@ -27,15 +27,28 @@ def netvar_main() -> None:
     unidriver_dll = UnidriverDLLWrapper(dll_path)
     unidriver_io = UnidriverIO(unidriver_dll)
     dev_fabric = UnidriverDeviceFabric(unidriver_dll)
-    dev = dev_fabric.create_modbus_udp_client('192.168.0.86', '5006', discr_inputs_size_byte=0,
+    dev = dev_fabric.create_modbus_udp_client('192.168.0.81', '5006', discr_inputs_size_byte=0,
                                               coils_size_byte=0, hold_regs_reg=20, input_regs_reg=0)
     var_fabric = NetVarFabric(unidriver_io, dev)
-    var: NetVar[int] = var_fabric.make('', NetVarCTypes.U8, index=NetVarIndex(33))
-    var.set(5)
-    while True:
-        unidriver_io.tick()
+
+    repo = NetVarRepo(unidriver_io, 0)
+    repo.push_back(NetVarCTypes.U8)
+    repo.push_back(NetVarCTypes.U8)
+    repo.push_back(NetVarCTypes.BIT)
+    repo.push_back(NetVarCTypes.BIT)
+    repo.push_back(NetVarCTypes.U32)
+    repo.push_back(NetVarCTypes.U8)
+    repo.print()
+    print('--')
+    repo.replace(2, NetVarCTypes.U16)
+    repo.print()
+    # var: NetVar[int] = var_fabric.make('', NetVarCTypes.U8, index=NetVarIndex(33))
+    # var.set(5)
+    # while True:
+    #     unidriver_io.tick()
+    #     print(var.get())
 
 
 if __name__ == '__main__':
-    builder_main()
-    # netvar_main()
+    # builder_main()
+    netvar_main()
